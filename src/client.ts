dd5dcf75040c21349c5bc72077974ad554c3d51a
Mqx <62719703+Mqxx@users.noreply.github.com>
2026-01-31 16:29:26 +0100

    fix: renamed

--- src/client.ts
+++ src/client.ts
@@ -2,7 +2,7 @@ import jsonRpcApiSchema from './json-rpc-api-schema.ts';
 import { ExtractMethod } from './schema_structure/extract_method.ts';
 import { ExtractMethodNames } from './schema_structure/extract_method_names.ts';
 import { ExtractResult } from './schema_structure/extract_result.ts';
-import { ExtractParams } from './schema_structure/extreact_params.ts';
+import { ExtractParams } from './schema_structure/extract_params.ts';
 import { OpenRpcSchema } from './schema_structure/open_rpc_schema.ts';
 
 export class Client<Schema extends OpenRpcSchema> {
